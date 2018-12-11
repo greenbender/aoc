8fb7b5ef47ef8216e3b9600871117091bf4949a3
greenbender <byron.platt@gmail.com>
2018-12-12 02:46:28 +1100

    Change to summed area table solution
    Its much faster (even without a hueristic to stop early)

--- day11.py
+++ day11.py
@@ -15,51 +15,52 @@ def level(x, y):
     return value - 5
 
 
-# build basic grid
-grid = []
-for y in range(300):
-    grid.append([0] * 300)
-    for x in range(300):
-        grid[y][x] = level(x + 1, y + 1)
+# build summed area table
+table = []
+for y in range(301):
+    table.append([0] * 301)
+    if not y:
+        continue
+    for x in range(301):
+        if not x:
+            continue
+        table[y][x] = \
+            level(x, y) + \
+            table[y - 1][x] + \
+            table[y][x - 1] - \
+            table[y - 1][x - 1]
 
 
-# iterate over larger block sizes updating grid
-gp = None
-gs = gx = gy = 0
-for size in range(2, 301):
-    bp = None
-    bx = by = 0
-    for y in range(300 - size):
-        for x in range(300 - size):
-            for dy in range(1, size + 1):
-                grid[y][x] += level(x + size, y + dy)
-            for dx in range(1, size):
-                grid[y][x] += level(x + dx, y + size)
+def most_power(size):
+    mp = None
+    mx = my = 0
+    for y in range(1, 301 - size):
+        for x in range(1, 301 - size):
+            power = \
+                table[y + size - 1][x + size - 1] + \
+                table[y - 1][x - 1] - \
+                table[y - 1][x + size - 1] - \
+                table[y + size - 1][x - 1]
+            if mp is None or power > mp:
+                mp = power
+                mx = x
+                my = y
+    return mp, mx, my
 
-            # keep track of best block
-            if bp is None or grid[y][x] > bp:
-                bp = grid[y][x]
-                bx = x + 1
-                by = y + 1
 
-    # keep track of best block across all sizes 
-    if bp is None or bp > gp:
-        gp = bp
-        gs = size
-        gx = bx
-        gy = by
-
-    # part 1
-    if size == 3:
-        print "%d,%d" % (bx, by)
-
-    # since power levels tend to be negative on average since (0-5=-5 and
-    # 9-5=4) when block sizes reach a certain size the total power for a block
-    # will be negative. As such when we first find that all blocks of a given
-    # size have power levels at or below zero we can stop searching
-    if bp < 0:
-        break
+# part 1
+_, x, y = most_power(3)
+print '%d,%d' % (x, y)
 
 
 # part 2
-print "%d,%d,%d" % (gx, gy, gs)
+mp = None
+mx = my = ms = 0
+for size in range(1, 301):
+    p, x, y = most_power(size)
+    if mp is None or p > mp:
+        mp = p
+        mx = x
+        my = y
+        ms = size
+print '%d,%d,%d' % (mx, my, ms)
