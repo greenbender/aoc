c4c2b5076d3f785f4cf594f5d60c3c9d41da71ca
greenbender <byron.platt@gmail.com>
2018-12-16 10:31:05 +1100

    Small tidyup for Day 13

--- day13.py
+++ day13.py
@@ -5,6 +5,7 @@ import sys
 
 
 class Cart(object):
+
     def __init__(self, x, y, dx, dy):
         self.x = x
         self.y = y
@@ -12,31 +13,36 @@ class Cart(object):
         self.dy = dy
         self.turn = 0
 
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
 
-class Element(object):
+
+class Straight(object):
+
     def modify(self, cart):
-        cart.x += cart.dx
-        cart.y += cart.dy
+        pass
 
 
-class Curve(Element):
+class Curve(Straight):
     def __init__(self, transform):
         self.transform = transform
+
     def modify(self, cart):
         t = self.transform
         cart.dx, cart.dy = (
             cart.dx * t[0] - cart.dy * t[1],
             cart.dx * t[2] + cart.dy * t[3]
         )
-        super(Curve, self).modify(cart)
 
 
-class Intersection(Element):
+class Intersection(Straight):
     transforms = [
         [0,-1,-1, 0],
         [1, 0, 0, 1],
         [0, 1, 1, 0],
     ]
+
     def modify(self, cart):
         t = self.transforms[cart.turn]
         cart.dx, cart.dy = (
@@ -44,96 +50,94 @@ class Intersection(Element):
             cart.dx * t[2] + cart.dy * t[3]
         )
         cart.turn = (cart.turn + 1) % 3
-        super(Intersection, self).modify(cart)
 
 
 # singletons
-straight = Element()
+straight = Straight()
 curve_bs = Curve([0,-1, 1, 0])
 curve_fs = Curve([0, 1,-1, 0])
 intersection = Intersection()
 
 
-# keep track of where the carts are
-class State(object):
-    def __init__(self, element, cart=None):
-        self.element = element
-        self.cart = cart
 
+class Track(object):
 
-def parse_track(fd):
-    carts = []
-    track = [[None] * 150 for i in range(150)]
-    for y, line in enumerate(fd):
-        for x, c in enumerate(line):
-            if c == '\\':
-                track[y][x] = State(curve_bs)
-            elif c == '/':
-                track[y][x] = State(curve_fs)
-            elif c == '+':
-                track[y][x] = State(intersection)
-            elif c in ['-', '|']:
-                track[y][x] = State(straight)
-            elif c == '>':
-                cart = Cart(x, y, 1, 0)
-                carts.append(cart)
-                track[y][x] = State(straight, cart)
-            elif c == '<':
-                cart = Cart(x, y, -1, 0)
-                carts.append(cart)
-                track[y][x] = State(straight, cart)
-            elif c == '^':
-                cart = Cart(x, y, 0, -1)
-                carts.append(cart)
-                track[y][x] = State(straight, cart)
-            elif c == 'v':
-                cart = Cart(x, y, 0, 1)
-                carts.append(cart)
-                track[y][x] = State(straight, cart)
-    return carts, track
+    def __init__(self, carts, track, state):
+        self.carts = carts
+        self.track = track
+        self.state = state
 
-
-def drive(carts, track):
-    first_crash = True
-    while True:
-        crashed_carts = []
-        for cart in sorted(carts, key=lambda c: (c.y, c.x)):
-
-            if cart in crashed_carts:
+    def tick(self):
+        skip = []
+        for cart in sorted(self.carts, key=lambda c: (c.y, c.x)):
+            if cart in skip:
                 continue
 
-            # move the cart 
-            s0 = track[cart.y][cart.x]
-            s0.element.modify(cart)
-            s0.cart = None
+            self.state[cart.y][cart.x] = None
+            cart.move()
 
-            # check for a crash
-            s1 = track[cart.y][cart.x]
-            if s1.cart:
-            
-                # part 1
-                if first_crash:
-                    first_crash = False
-                    print '%d,%d' % (cart.x, cart.y)
+            # crash
+            victim = self.state[cart.y][cart.x]
+            if victim:
+                self.state[cart.y][cart.x] = None
+                crashed = (victim, cart)
+                for c in crashed:
+                    skip.append(c)
+                    self.carts.remove(c)
+                yield crashed
 
-                # remove carts
-                crashed_carts.extend([cart, s1.cart])
-                s1.cart = None
-
-            # update the track state
             else:
-                s1.cart = cart
+                self.state[cart.y][cart.x] = cart
+                self.track[cart.y][cart.x].modify(cart)
 
-        # remove crashed carts
-        for cart in crashed_carts:
-            carts.remove(cart)
+    @classmethod
+    def load(cls, fd):
+        carts = []
+        track = [[None] * 150 for i in range(150)]
+        state = [[None] * 150 for i in range(150)]
+        for y, line in enumerate(fd):
+            for x, c in enumerate(line):
+                if c == '\\':
+                    track[y][x] = curve_bs
+                elif c == '/':
+                    track[y][x] = curve_fs
+                elif c == '+':
+                    track[y][x] = intersection
+                elif c in ['-', '|']:
+                    track[y][x] = straight
+                elif c == '>':
+                    cart = Cart(x, y, 1, 0)
+                    carts.append(cart)
+                    track[y][x] = straight
+                    state[y][x] = cart
+                elif c == '<':
+                    cart = Cart(x, y, -1, 0)
+                    carts.append(cart)
+                    track[y][x] = straight
+                    state[y][x] = cart
+                elif c == '^':
+                    cart = Cart(x, y, 0, -1)
+                    carts.append(cart)
+                    track[y][x] = straight
+                    state[y][x] = cart
+                elif c == 'v':
+                    cart = Cart(x, y, 0, 1)
+                    carts.append(cart)
+                    track[y][x] = straight
+                    state[y][x] = cart
+        return cls(carts, track, state)
 
-        # part 2
-        if len(carts) == 1:
-            print '%d,%d' % (carts[0].x, carts[0].y)
-            break
-    
+
+first = True
+track = Track.load(sys.stdin)
+while len(track.carts) > 1:
+    for victim, cart in track.tick():
+
+        # part 1
+        if first:
+            first = False
+            print '%d,%d' % (cart.x, cart.y)
 
 
-carts, track = parse_track(sys.stdin)
-drive(carts, track)
+# part 2
+print '%d,%d' % (track.carts[0].x, track.carts[0].y)
