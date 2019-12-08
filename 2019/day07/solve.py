6d1506d9e922dc6117a795347cbb7b31e1e2610c
greenbender <byron.platt@gmail.com>
2019-12-09 04:18:58 +1100

    Day 7 Tidy

--- 2019/day07/solve.py
+++ 2019/day07/solve.py
@@ -10,21 +10,30 @@ if len(sys.argv) != 2:
 prog = map(int, open(sys.argv[1], 'rb').read().split(','))
 
 
-class IntCoder(object):
+class IntIO(object):
 
-    def __init__(self, prog, phase):
-        self.prog = prog
-        self.pc = 0
-        self.inputs = [phase]
-        self.attached = None
-        self.signal = None
+    def __init__(self):
+        self.buf = []
+
+    def read(self):
+        if self.buf:
+            return self.buf.pop()
+        return None
+
+    def write(self, v):
+        self.buf.insert(0, v)
+        
+
+class IntRunner(object):
+
+    def __init__(self, prog, stdin, stdout):
+        self.prog = list(prog)
+        self.stdin = stdin
+        self.stdout = stdout
+        self.last_output = None
+        self.exit_code = None
         self.done = False
-
-    def attach(self, coder):
-        self.attached = coder
-
-    def add_input(self, value):
-        self.inputs.append(value)
+        self.pc = 0
 
     @staticmethod
     def immediate(mod, n):
@@ -41,127 +50,165 @@ class IntCoder(object):
             raise Exception('add: parameter 2 must use position mode')
         o = [self.operand(mod, n) for n in range(3)]
         self.prog[o[2]] = self.prog[o[0]] + self.prog[o[1]]
-        return self.pc + 3
+        self.pc += 3
 
     def mul(self, mod):
         if self.immediate(mod, 2):
             raise Exception('mul: parameter 2 must use position mode')
         o = [self.operand(mod, n) for n in range(3)]
         self.prog[o[2]] = self.prog[o[0]] * self.prog[o[1]]
-        return self.pc + 3
+        self.pc += 3
 
     def get(self, mod):
-        if not self.inputs:
-            return self.pc - 1
+        v = self.stdin.read()
+        if v is None:
+            self.pc -= 1 # wait for input (move pc back to get opcode)
         else:
-            v = self.inputs.pop(0)
             self.prog[self.operand(mod, 0)] = v
-            return self.pc + 1
+            self.pc += 1
 
     def put(self, mod):
         v = self.prog[self.operand(mod, 0)]
-        self.signal = v
-        if self.attached:
-            self.attached.add_input(v)
-        else:
-            print "Output:", v
-        return self.pc + 1
+        self.last_output = v
+        self.stdout.write(v)
+        self.pc += 1
 
     def jnz(self, mod):
         o = [self.operand(mod, n) for n in range(2)]
         if self.prog[o[0]]:
-            return self.prog[o[1]]
-        return self.pc + 2
+            self.pc = self.prog[o[1]]
+        else:
+            self.pc += 2
 
     def jz(self, mod):
         o = [self.operand(mod, n) for n in range(2)]
         if not self.prog[o[0]]:
-            return self.prog[o[1]]
-        return self.pc + 2
+            self.pc = self.prog[o[1]]
+        else:
+            self.pc += 2
 
     def lt(self, mod):
         if self.immediate(mod, 2):
             raise Exception('lt: parameter 2 must use position mode')
         o = [self.operand(mod, n) for n in range(3)]
         self.prog[o[2]] = 1 if self.prog[o[0]] < self.prog[o[1]] else 0
-        return self.pc + 3
+        self.pc += 3
 
     def eq(self, mod):
         if self.immediate(mod, 2):
             raise Exception('eq: parameter 2 must use position mode')
         o = [self.operand(mod, n) for n in range(3)]
         self.prog[o[2]] = 1 if self.prog[o[0]] == self.prog[o[1]] else 0
-        return self.pc + 3
+        self.pc += 3
 
+    def end(self):
+        self.done = True
+        self.exit_code = self.prog[0]
 
     @staticmethod
-    def getop(v):
+    def parse(v):
         mod, opcode = divmod(v, 100)
         mod, b0 = divmod(mod, 10)
         mod, b1 = divmod(mod, 10)
         mod, b2 = divmod(mod, 10)
         if b0 > 1 or b1 > 1 or b2 > 1:
-            raise Exception('Invalid mode')
+            raise Exception('getop: Invalid mode')
         mod = b2 << 2 | b1 << 1 | b0
         return opcode, mod
 
-
     def step(self):
-        opcode, mod = self.getop(self.prog[self.pc])
+        opcode, mod = self.parse(self.prog[self.pc])
         self.pc += 1
         if opcode == 99:
-            self.done = True
-            return True
+            self.end()
+            return False
         elif opcode == 1:
-            self.pc = self.add(mod)
+            self.add(mod)
         elif opcode == 2:
-            self.pc = self.mul(mod)
+            self.mul(mod)
         elif opcode == 3:
-            self.pc = self.get(mod)
+            self.get(mod)
         elif opcode == 4:
-            self.pc = self.put(mod)
+            self.put(mod)
         elif opcode == 5:
-            self.pc = self.jnz(mod)
+            self.jnz(mod)
         elif opcode == 6:
-            self.pc = self.jz(mod)
+            self.jz(mod)
         elif opcode == 7:
-            self.pc = self.lt(mod)
+            self.lt(mod)
         elif opcode == 8:
-            self.pc = self.eq(mod)
+            self.eq(mod)
         else:
-            raise Exception('Program error at %d: %d' % (pc, p[pc]))
-        return False
+            raise Exception('step: Program error at %d: %d' % (self.pc, opcode))
+        return True
 
     def run(self):
+        while self.step():
+            pass
+        return self.exit_code
+
+
+def part1():
+    outputs = []
+    for c in itertools.permutations(range(5)):
+
+        # create 6 IntIO's
+        ios = []
+        for i in range(6):
+            ios.append(IntIO())
+     
+        # create 5 amplifiers and connect their stdin/stdout
+        amps = []
+        for i in range(5):
+            amp = IntRunner(prog, ios[i], ios[i + 1])
+            amp.stdin.write(c[i])
+            amps.append(amp)
+
+        # initialise first amplifier
+        amps[0].stdin.write(0)
+
+        # run amplifier to get final output
         while True:
-            if self.step():
+            if amps[4].done:
+                outputs.append(amps[4].last_output)
                 break
+            for amp in amps:
+                if not amp.done:
+                    amp.step()
+
+    print max(outputs)
 
 
-outputs = []
-for c in itertools.permutations(range(5, 10)):
+def part2():
+    outputs = []
+    for c in itertools.permutations(range(5, 10)):
 
-    print c
+        # create 5 IntIO's
+        ios = []
+        for i in range(5):
+            ios.append(IntIO())
+     
+        # create 5 amplifiers and connect their stdin/stdout
+        amps = []
+        for i in range(5):
+            amp = IntRunner(prog, ios[i], ios[(i + 1) % 5])
+            amp.stdin.write(c[i])
+            amps.append(amp)
 
-    amps = []
-    for i in range(5):
-        amps.append(IntCoder(list(prog), c[i]))
+        # initialise first amplifier
+        amps[0].stdin.write(0)
 
-    amps[0].attach(amps[1])
-    amps[1].attach(amps[2])
-    amps[2].attach(amps[3])
-    amps[3].attach(amps[4])
-    amps[4].attach(amps[0])
+        # run amplifier to get final output
+        while True:
+            if amps[4].done:
+                outputs.append(amps[4].last_output)
+                break
+            for amp in amps:
+                if not amp.done:
+                    amp.step()
 
-    amps[0].add_input(0)
+    print max(outputs)
 
-    while True:
-        if amps[4].done:
-            outputs.append(amps[4].signal)
-            print amps[4].signal
-            break
-        for amp in amps:
-            if not amp.done:
-                amp.step()
 
-print max(outputs)
+part1()
+part2()
