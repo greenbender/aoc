439c5bc9468f98f87c29255a6c71f08aa66d8805
greenbender <byron.platt@gmail.com>
2023-12-04 17:15:51 +1100

    Add typing

--- 2023/day04/solve.py
+++ 2023/day04/solve.py
@@ -2,18 +2,18 @@ import sys
 from functools import cache
 
 
-def parse(line):
+def parse(line: str) -> tuple[set[int], set[int]]:
     _, numbers = line.split(": ")
-    winners, mine = numbers.split(" | ")
-    winners = {int(x) for x in winners.split()}
-    mine = {int(x) for x in mine.split()}
+    left, right = numbers.split(" | ")
+    winners = {int(x) for x in left.split()}
+    mine = {int(x) for x in right.split()}
     return winners, mine
 
 
 cards = [parse(line.strip()) for line in sys.stdin]
 
 
-def part1():
+def part1() -> None:
     total = 0
     for winners, mine in cards:
         count = len(winners & mine)
@@ -26,7 +26,7 @@ part1()
 
 
 @cache
-def cards_from(i):
+def cards_from(i: int) -> int:
     winners, mine = cards[i]
     count = len(winners & mine)
     start = i + 1
@@ -34,7 +34,7 @@ def cards_from(i):
     return count + sum(cards_from(j) for j in range(start, end))
 
 
-def part2():
+def part2() -> None:
     print(sum(cards_from(i) + 1 for i in range(len(cards))))
 
 
